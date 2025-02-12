bc8cc1c4313df1be4643450e632fbbb66dae0f48
Davanum Srinivas <davanum@gmail.com>
2025-02-11 20:24:13 -0500

    Do not add placement group automatically when capacity reservation is set
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- pkg/cfn/builder/managed_launch_template.go
+++ pkg/cfn/builder/managed_launch_template.go
@@ -76,7 +76,7 @@ func (m *ManagedNodeGroupResourceSet) makeLaunchTemplateData(ctx context.Context
 		if err := buildNetworkInterfaces(ctx, launchTemplateData, mng.InstanceTypeList(), true, securityGroupIDs, m.ec2API); err != nil {
 			return nil, fmt.Errorf("couldn't build network interfaces for launch template data: %w", err)
 		}
-		if mng.Placement == nil {
+		if mng.Placement == nil && mng.CapacityReservation == nil {
 			groupName := m.newResource("NodeGroupPlacementGroup", &gfnec2.PlacementGroup{
 				Strategy: gfnt.NewString("cluster"),
 			})
